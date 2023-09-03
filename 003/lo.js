0ead0809004eefbd104fc4c019bac69a266f1f39
loslsl <105191922+loslsl@users.noreply.github.com>
2023-09-03 13:51:53 -0600

    Delete 003 directory

--- 003/lo.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-var elems = document.querySelectorAll('.parallax');
-var instances = M.Parallax.init(elems);
-});
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.pushpin');
-    var instances = M.Pushpin.init(elems);
-  });
